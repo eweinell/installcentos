ddd106e7c3a3a647fb2d63827283e46306c13c50
Erhard Weinell <erhard.weinell@generali.com>
2019-01-19 17:42:01 +0100

    updated installer versions

---
+++ install-bootstrapped.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+## see: https://youtu.be/aqXSbDZggK4
+
+. ./user-custom-exports.sh
+
+## Default variables to use
+export INTERACTIVE=${INTERACTIVE:="true"}
+export PVS=${INTERACTIVE:="true"}
+export DOMAIN=${DOMAIN:="$(curl -s ipinfo.io/ip).nip.io"}
+export USERNAME=${USERNAME:="$(whoami)"}
+export PASSWORD=${PASSWORD:=password}
+export VERSION=${VERSION:="3.11"}
+export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/gshipley/installcentos/master"}
+export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
+export API_PORT=${API_PORT:="8443"}
+
+## Make the script interactive to set the variables
+if [ "$INTERACTIVE" = "true" ]; then
+	read -rp "Domain to use: ($DOMAIN): " choice;
+	if [ "$choice" != "" ] ; then
+		export DOMAIN="$choice";
+	fi
+
+	read -rp "Username: ($USERNAME): " choice;
+	if [ "$choice" != "" ] ; then
+		export USERNAME="$choice";
+	fi
+
+	read -rp "Password: ($PASSWORD): " choice;
+	if [ "$choice" != "" ] ; then
+		export PASSWORD="$choice";
+	fi
+
+	read -rp "OpenShift Version: ($VERSION): " choice;
+	if [ "$choice" != "" ] ; then
+		export VERSION="$choice";
+	fi
+	read -rp "IP: ($IP): " choice;
+	if [ "$choice" != "" ] ; then
+		export IP="$choice";
+	fi
+
+	read -rp "API Port: ($API_PORT): " choice;
+	if [ "$choice" != "" ] ; then
+		export API_PORT="$choice";
+	fi 
+
+	echo
+
+fi
+
+echo "******"
+echo "* Your domain is $DOMAIN "
+echo "* Your IP is $IP "
+echo "* Your username is $USERNAME "
+echo "* Your password is $PASSWORD "
+echo "* OpenShift version: $VERSION "
+echo "******"
+
+# install updates
+yum update -y
+
+# install the following base packages
+yum install -y  git
+
+if [ ! -e ./installer ]; then
+  git clone https://github.com/eweinell/installcentos.git ./installer
+  cd ./installer 
+else 
+  cd ./installer
+  git pull
+fi
+
+export INTERACTIVE=false
+./install-openshift.sh
--- install-istio/install-istio.sh
+++ install-istio/install-istio.sh
@@ -6,26 +6,29 @@ echo 'vm.max_map_count=262144' > /etc/sysctl.d/99-elasticsearch.conf
 
 # enable operator hooks
 cd /etc/origin/master/
-cp -p master-config.yaml master-config.yaml.prepatch
-oc ex config patch master-config.yaml.prepatch -p 'admissionConfig:
-  pluginConfig:
-    MutatingAdmissionWebhook:
-      configuration:
-        apiVersion: apiserver.config.k8s.io/v1alpha1
-        kubeConfigFile: /dev/null
-        kind: WebhookAdmission
-    ValidatingAdmissionWebhook:
-      configuration:
-        apiVersion: apiserver.config.k8s.io/v1alpha1
-        kubeConfigFile: /dev/null
-        kind: WebhookAdmission' > master-config.yaml
-master-restart api
-master-restart controllers
+if [ ! -e master-config.yaml.prepatch ]; then
+	cp -p master-config.yaml master-config.yaml.prepatch
+	oc ex config patch master-config.yaml.prepatch -p 'admissionConfig:
+	  pluginConfig:
+	    MutatingAdmissionWebhook:
+	      configuration:
+	        apiVersion: apiserver.config.k8s.io/v1alpha1
+	        kubeConfigFile: /dev/null
+	        kind: WebhookAdmission
+	    ValidatingAdmissionWebhook:
+	      configuration:
+	        apiVersion: apiserver.config.k8s.io/v1alpha1
+	        kubeConfigFile: /dev/null
+	        kind: WebhookAdmission' > master-config.yaml
+	master-restart api
+	master-restart controllers
+	sleep 15
+fi
 
 # add operator project & install
 oc new-project istio-operator
-sleep 5
-curl https://raw.githubusercontent.com/Maistra/openshift-ansible/maistra-0.5/istio/istio_product_operator_template.yaml |oc process -f- --param=OPENSHIFT_ISTIO_MASTER_PUBLIC_URL=https://console.192.168.178.130.nip.io:8443 --param=OPENSHIFT_ISTIO_PREFIX=registry.access.redhat.com/openshift-istio-tech-preview/ --param=OPENSHIFT_RELEASE=v3.11.0 | oc create -f- -n istio-operator
+#curl https://raw.githubusercontent.com/Maistra/openshift-ansible/maistra-0.5/istio/istio_product_operator_template.yaml |oc process -f- --param=OPENSHIFT_ISTIO_MASTER_PUBLIC_URL=https://console.192.168.178.130.nip.io:8443 --param=OPENSHIFT_ISTIO_PREFIX=registry.access.redhat.com/openshift-istio-tech-preview/ --param=OPENSHIFT_RELEASE=v3.11.0 | oc create -f- -n istio-operator
+curl https://raw.githubusercontent.com/Maistra/openshift-ansible/maistra-0.6/istio/istio_product_operator_template.yaml |oc process -f- --param=OPENSHIFT_ISTIO_MASTER_PUBLIC_URL=https://console.192.168.178.130.nip.io:8443 --param=OPENSHIFT_ISTIO_PREFIX=registry.access.redhat.com/openshift-istio-tech-preview/ --param=OPENSHIFT_RELEASE=v3.11.0 | oc create -f- -n istio-operator
 
 # add demo project
 oc new-project myproject
