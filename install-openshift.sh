147e95784f9527b92aecaff71d6c0118aafbc490
Erhard Weinell <erhard.weinell@generali.com>
2019-01-28 11:11:54 +0100

    temp-fix new ansible

--- install-openshift.sh
+++ install-openshift.sh
@@ -154,8 +154,8 @@ fi
 mkdir -p /etc/origin/master/
 touch /etc/origin/master/htpasswd
 
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
-ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml  || exit 1
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml || exit 1
 
 htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
